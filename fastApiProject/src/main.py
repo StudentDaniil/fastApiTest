505e2e94cf478fa181d507f14eb1321cee837067
StudentDaniil <99341865+StudentDaniil@users.noreply.github.com>
2024-08-04 23:54:02 +0300

    Update main.py

--- fastApiProject/src/main.py
+++ fastApiProject/src/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter, HTTPException
-from configurations import collection
+from configurations import collection_message
 from database.schemas import all_data
 from database.models import Message
 
@@ -8,17 +8,20 @@ app = FastAPI()
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/api/v1/messages/")
 async def get_all_message():
-    data = collection.find()
+    data = collection_message.find()
 
     return all_data(data)
 
 
-@router.post("/")
+@router.post("/api/v1/message/")
 async def create_message(new_message: Message):
     try:
-        resp = collection.insert_one(dict(new_message))
+        message_dict = new_message.dict()
+        from_f_dict = new_message.from_f.to_dict()
+        message_dict["from_f"] = from_f_dict
+        resp = collection_message.insert_one(message_dict)
         return {"status_code": 200, "id": str(resp.inserted_id)}
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Error: {e}")
